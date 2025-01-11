bb545abf5e5c927df609babd5084bc926e7cdc50
AKHIL V A <akhilva873@gmail.com>
2025-01-11 19:28:10 +0530

    simulate

---
+++ shared_memory/sensor_reader_simulate.py
@@ -0,0 +1,13 @@
+import time
+from shared_memory_util import write_data_to_shared_memory
+
+try:
+    while True:
+        rpm = input("Enter the pressure: ")
+        # Convert the input to a number (int or float depending on the requirement)
+        rpm = float(rpm)  # Use int() if you expect integer RPM values
+        write_data_to_shared_memory("Water_Level", rpm)
+        time.sleep(1)  # Measurement interval of 1 second
+
+except KeyboardInterrupt:
+    print("Measurement stopped by user")
